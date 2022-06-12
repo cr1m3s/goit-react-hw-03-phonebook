0e2fdfad733156f70e48fca4336513b46ed060aa
cr1m3s <parioleks@gmail.com>
2022-06-12 17:48:27 +0200

    done?

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
 import style from './App.module.css';
 
+const LS_KEY = 'contacts';
+
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    if (JSON.parse(localStorage.getItem(LS_KEY))) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem(LS_KEY)) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
